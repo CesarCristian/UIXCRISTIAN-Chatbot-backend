2397b5de206a9eacd1cd2590033df07c83e7ee0c
Cesar Cristian Silva Castromonte <nevaspherecorp@gmail.com>
2026-08-09 15:35:27 -0500

    Permitir CORS universal para Vercel en index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'http://localhost:4200'
-}));
+app.use(cors());
 app.use(express.json());
 
 const apiKey = process.env.GEMINI_API_KEY;
